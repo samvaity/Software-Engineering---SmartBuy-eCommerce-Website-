961018b49490a152c5ff03c9c310ea53db909c2d
samvaity <samvaity@indiana.edu>
2016-11-03 18:06:05 -0400

    Login Commit

---
+++ app.js
@@ -0,0 +1,66 @@
+/*var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/login.html', function (req, res) {
+  //console.log( process.cwd());
+   res.sendFile( process.cwd() + "/public/views/" + "login.html" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   //console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+
+})*/
+
+var express  = require('express');
+var app      = express();
+var port     = process.env.PORT || 8080;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var path = require('path'), fs = require('fs');
+var http = require('http')
+var server = http.createServer(app)
+
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url); 
+
+require('./config/passport')(passport); 
+
+app.configure(function() {
+
+   app.use(express.cookieParser());
+   app.use(express.bodyParser()); 
+   app.use(express.static(path.join(__dirname, 'public')));
+   app.set('views', __dirname + '/views');
+   app.engine('html', require('ejs').renderFile);
+   app.use(express.session({ secret: 'knoldus' })); 
+   /*app.use(express.bodyParser({uploadDir:'/images'}));*/
+   app.use(passport.initialize());
+   app.use(passport.session()); 
+   app.use(flash()); 
+
+});
+
+
+require('./app/routes.js')(app, passport,server); 
+
+server.listen(port);
+console.log('Listening  to  port ' + port);
+
+
---
+++ public/js/login.js
@@ -0,0 +1,77 @@
+(function( $ ){
+	   
+  // Easing equation based on
+  // EaseInOutExpo by Robert Penner (c) 2001
+  // robertpenner.com/easing_terms_of_use.html
+  
+  $.fn.extend( jQuery.easing, {
+    eioe: function( ø, t, b, c, d ) {
+      if(t==0) return b;
+      if(t==d) return b+c;
+      if( (t /= d/2) < 1 ) return c/2 * Math.pow( 2, 10 * (t - 1) ) + b;
+      return c/2 * ( -Math.pow( 2, -10 * --t ) + 2 ) + b;
+    }
+  });
+  
+  // Toggle disabled
+  // http://stackoverflow.com/questions/4702000/jquery-toggle-input-disabled-attribute#comment5189637_4702086
+  
+  $.fn.toggleDisabled = function() {
+    return this.each(function() {
+      this.disabled = !this.disabled;
+    });
+  };
+  
+  // Toggle attribute value
+  // Anders Grimsrud, 2013
+  
+  $.fn.toggleAttr = function(a, v1, v2) {
+    return this.each(function() {
+      var $t = $(this),
+          v  = $t.attr(a) === v1 ? v2 : v1;
+      $t.attr(a, v)
+        });
+  };
+  
+  // Toggle login/register form
+  
+  $('.register').click(function(){
+    
+    // Toggle register form and enable inputs
+    $('.register-form').slideToggle({
+      easing: 'eioe',
+      duration: 250
+    }).find('input').toggleDisabled();
+    
+    // Change header
+    // Login -> Register
+    var $h2 = $('.box h2'),
+        headerText = $h2.text() === "Login" 
+        ? "Register" 
+        : "Login";
+    $h2.text(headerText);
+    
+    // Change submit button value
+    // Login -> Register
+    $('#submit').toggleAttr('value','Login','Register');
+    
+    // Change signup link
+    // Signup -> Login link
+    var $su = $('.register');
+    $su.toggleAttr('href','register.htm','login.htm')
+      var signupLinkText = $su.text() === "Register!" 
+          ? "Login!" 
+          : "Register!";
+    $su.text(signupLinkText);
+    
+    // Hide Forgot password link
+    $('.forgot-password').toggle();
+    
+    // Change form action
+    // login.php -> register.php
+    $('form').toggleAttr('action','login.php','register.php')
+      
+      return false;
+  });
+  
+})(jQuery);
