788057c60959f5e64d6a6cabd5c21f2e5ee81a40
rrane <rrane@umail.iu.edu>
2016-11-12 16:18:41 -0500

    minor changes

--- app/commonserver.js
+++ app/commonserver.js
@@ -1,5 +1,7 @@
 /* This file contains all common javascript functions that can be used with other js files
 (server side only)*/
+
+/* This function will check if the user is logged in, if not, then redirect to login page */
 module.exports.auth = function (req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
