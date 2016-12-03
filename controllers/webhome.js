c75f1796250d2a26ea0adbdd45c22413c8b94073
bmerugur <bmerugur@indiana.edu>
2016-12-02 20:20:55 -0500

    Create webhome.js

---
+++ controllers/webhome.js
@@ -0,0 +1,18 @@
+var commonserver = require('./commonserver');
+module.exports = function(app) {
+
+
+//spl case for when user puts /login in the web url
+    app.get('/webhome',  function(request, response) {
+		//console.log('n');
+            response.render('webhome.html', { 
+	    	//for pagination
+	    	user: request.user,
+	        tagline: commonserver.getTagLine(request.user),
+	        nextPage: "#",
+	        searchtext: "",
+	        tags: commonserver.getTags(),
+	  		message: request.flash('error while loading data') 
+	  	});
+        });
+		}
